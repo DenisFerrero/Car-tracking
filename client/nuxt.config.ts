ca8ba7acd0fc4d1ede3ede9c43b6613e3cd7afc4
Denis Ferrero <denis.ferrero@tecnologic-to.it>
2025-02-09 15:32:43 +0100

    Custom server for client from env variables

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      server: 'http://localhost:4000'
+      server: process.env.SERVER || 'http://localhost:4000'
     }
   },
 
